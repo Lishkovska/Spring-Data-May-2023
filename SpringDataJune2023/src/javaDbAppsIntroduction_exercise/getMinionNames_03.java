b5237215997e52217efb53420df2a55cd74528b6
Lishkovska <114057232+Lishkovska@users.noreply.github.com>
2023-06-15 13:01:53 +0300

    add solutions

---
+++ SpringDataJune2023/src/javaDbAppsIntroduction_exercise/getMinionNames_03.java
@@ -0,0 +1,56 @@
+package javaDbAppsIntroduction_exercise;
+
+import java.sql.*;
+import java.util.Properties;
+import java.util.Scanner;
+
+public class getMinionNames_03 {
+    public static void main(String[] args) throws SQLException {
+        Scanner scanner = new Scanner(System.in);
+        Properties props = new Properties();
+        props.setProperty("user", "root");
+        props.setProperty("password", "Maria201118");
+
+        Connection connection = DriverManager
+                .getConnection("jdbc:mysql://localhost:3306/minions_db", props);
+
+        int villain_id = Integer.parseInt(scanner.nextLine());
+
+        PreparedStatement villainPrepStatement = connection.prepareStatement("select v.name  " +
+                "from villains as v " +
+                "where v.id = ? ;");
+
+
+        villainPrepStatement.setInt(1, villain_id);
+
+        ResultSet resultVillain = villainPrepStatement.executeQuery();
+
+        if(!resultVillain.next()){
+            System.out.printf("No villain with ID %d exists in the database.", villain_id);
+            return;
+        }
+
+        String villainName = resultVillain.getString("v.name");
+        System.out.printf("Villain: %s%n", villainName);
+
+        PreparedStatement minionPrepStatement = connection.prepareStatement("select m.name , m.age " +
+                "from minions as m " +
+                "join minions_villains as vm " +
+                "on vm.minion_id = m.id " +
+                "where vm.villain_id = ? ;");
+
+        minionPrepStatement.setInt(1, villain_id);
+        ResultSet resultMinion = minionPrepStatement.executeQuery();
+
+        for (int i = 1; resultMinion.next(); i++) {
+            System.out.printf("%d. %s %d%n", i,
+                    resultMinion.getString("m.name"),
+                    resultMinion.getInt("m.age"));
+        }
+
+
+
+
+        connection.close();
+    }
+}
---
+++ SpringDataJune2023/src/javaDbAppsIntroduction_exercise/getVillainName_02.java
@@ -0,0 +1,35 @@
+package javaDbAppsIntroduction_exercise;
+
+import java.sql.*;
+import java.util.Properties;
+import java.util.Scanner;
+
+public class getVillainName_02 {
+    public static void main(String[] args) throws SQLException {
+        Scanner scanner = new Scanner(System.in);
+        Properties props = new Properties();
+        props.setProperty("user", "root");
+        props.setProperty("password", "Maria201118");
+
+        Connection connection = DriverManager
+                .getConnection("jdbc:mysql://localhost:3306/minions_db", props);
+
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT v.name , " +
+                "count(distinct vm.minion_id) as count_minion " +
+                "from minions_villains as vm" +
+                " join villains as v " +
+                "on vm.villain_id = v.id " +
+                "group by v.name " +
+                "having count_minion > 15 " +
+                "order by count_minion desc;");
+
+        ResultSet result = preparedStatement.executeQuery();
+
+        while (result.next()){
+            System.out.println(result.getString("name") + " " +
+                    result.getString("count_minion"));
+        }
+
+        connection.close();
+    }
+}
